8b13074d4edbc1b581908369f2d56c7349531c8e
HennartCode <82595646+HennartCode@users.noreply.github.com>
2022-03-13 17:55:13 +0100

    Syntaxfehler

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 
     </head>
     <?php
-    session_start()
-    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    session_start();
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
       $loggedin = true;
     }else{
         $loggedin = false; 
